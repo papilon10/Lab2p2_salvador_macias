7c30c3ddc52c85e73b84d67b2ab5438946904b98
papilon10 <salvigna17@gmail.com>
2025-05-02 16:00:56 -0600

    explorar y menu admin hecho

--- src/lab2p2_salvador_macias/Lab2p2_salvador_macias.java
+++ src/lab2p2_salvador_macias/Lab2p2_salvador_macias.java
@@ -7,7 +7,9 @@ package lab2p2_salvador_macias;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//fila 3 silla 6
 /**
+ *
  *
  * @author Apple
  */
@@ -15,7 +17,7 @@ public class Lab2p2_salvador_macias {
 
     public static ArrayList<mapa> lista_mapas = new ArrayList();
     public static ArrayList<enemigo> lista_enemigos = new ArrayList();
-    public static ArrayList lista_heroes = new ArrayList();
+    public static ArrayList<heroe> lista_heroes = new ArrayList();
 
     public static Scanner lea = new Scanner(System.in);
     public static Scanner str = new Scanner(System.in);
@@ -82,39 +84,49 @@ public class Lab2p2_salvador_macias {
 
                                     break;
                                     case 3: {
-                                        listar_mapas();
-                                        System.out.println("ingrese el indice del mapa a modificar :");
-                                        int mod_mapa = lea.nextInt();
-                                        lista_mapas.get(mod_mapa);
-                                        System.out.println("ingrese el nombre de la zona: ");
-                                        String nombre = str.nextLine();
-                                        lista_mapas.get(mod_mapa).setZona(nombre);
-                                        System.out.println("ingrese el clima: ");
-                                        String clima = str.nextLine();
-                                        lista_mapas.get(mod_mapa).setClima(clima);
-                                        System.out.println("ingrese el efecto del clima en el heroe : ");
-                                        int efecto_clima_heroe = lea.nextInt();
-                                        lista_mapas.get(mod_mapa).setEfecto_clima_heroe(efecto_clima_heroe);
-                                        System.out.println("ingrese el efecto del clima en el enemigo :");
-                                        int efecto_clima_enemigo = lea.nextInt();
-                                        lista_mapas.get(mod_mapa).setEfecto_clima_enemigo(efecto_clima_enemigo);
+                                        if (lista_mapas.size() == 0) {
+                                            System.out.println("la lista de mapas esta vacia");
+                                        } else {
+                                            listar_mapas();
+                                            System.out.println("ingrese el indice del mapa a modificar :");
+                                            int mod_mapa = lea.nextInt();
+                                            lista_mapas.get(mod_mapa);
+                                            System.out.println("ingrese el nombre de la zona: ");
+                                            String nombre = str.nextLine();
+                                            lista_mapas.get(mod_mapa).setZona(nombre);
+                                            System.out.println("ingrese el clima: ");
+                                            String clima = str.nextLine();
+                                            lista_mapas.get(mod_mapa).setClima(clima);
+                                            System.out.println("ingrese el efecto del clima en el heroe : ");
+                                            int efecto_clima_heroe = lea.nextInt();
+                                            lista_mapas.get(mod_mapa).setEfecto_clima_heroe(efecto_clima_heroe);
+                                            System.out.println("ingrese el efecto del clima en el enemigo :");
+                                            int efecto_clima_enemigo = lea.nextInt();
+                                            lista_mapas.get(mod_mapa).setEfecto_clima_enemigo(efecto_clima_enemigo);
+
+                                        }
 
                                     }
 
                                     break;
                                     case 4: {
-                                        listar_enemigos();
-                                        System.out.println("ingrese el indice del enemigo a modificar : ");
-                                        int mod_enemigo = lea.nextInt();
-                                        System.out.println("ingrese el nombre del enemigo :");
-                                        String nombre = str.nextLine();
-                                        lista_enemigos.get(mod_enemigo).setNombre(nombre);
-                                        System.out.println("ingrese la vida: ");
-                                        int vida = lea.nextInt();
-                                        lista_enemigos.get(mod_enemigo).setVida(vida);
-                                        System.out.println("ingrese el ataque: ");
-                                        int ataque = lea.nextInt();
-                                        lista_enemigos.get(mod_enemigo).setAtaque(ataque);
+                                        if (lista_enemigos.size() == 0) {
+                                            System.out.println("la lista de enemigos esta vacia");
+                                        } else {
+                                            listar_enemigos();
+                                            System.out.println("ingrese el indice del enemigo a modificar : ");
+                                            int mod_enemigo = lea.nextInt();
+                                            System.out.println("ingrese el nombre del enemigo :");
+                                            String nombre = str.nextLine();
+                                            lista_enemigos.get(mod_enemigo).setNombre(nombre);
+                                            System.out.println("ingrese la vida: ");
+                                            int vida = lea.nextInt();
+                                            lista_enemigos.get(mod_enemigo).setVida(vida);
+                                            System.out.println("ingrese el ataque: ");
+                                            int ataque = lea.nextInt();
+                                            lista_enemigos.get(mod_enemigo).setAtaque(ataque);
+
+                                        }
 
                                     }
 
@@ -131,11 +143,29 @@ public class Lab2p2_salvador_macias {
 
                                     break;
                                     case 7: {
+                                        if (lista_mapas.size() == 0) {
+                                            System.out.println("la lista de mapas esta vacia");
+                                        } else {
+                                            listar_mapas();
+                                            System.out.println("ingrese el indice del mapa que desea borrar :");
+                                            int borrar_mapa = lea.nextInt();
+                                            lista_mapas.remove(borrar_mapa);
+
+                                        }
 
                                     }
 
                                     break;
                                     case 8: {
+                                        if (lista_enemigos.size() == 0) {
+                                            System.out.println("la lista de enemigos esta vacia");
+                                        } else {
+                                            listar_enemigos();
+                                            System.out.println("ingrese el indice del enemigo que desea borrar :");
+                                            int borrar_enemigo = lea.nextInt();
+                                            lista_mapas.remove(borrar_enemigo);
+
+                                        }
 
                                     }
 
@@ -156,7 +186,29 @@ public class Lab2p2_salvador_macias {
 
                         break;
                         case 2: {
-                            System.out.println("e");
+                            boolean salida_jugador = false;
+                            do {
+                                System.out.println("1.explorar");
+                                System.out.println("0.volver al menu principal");
+                                int opc_jugador = lea.nextInt();
+                                if (opc_jugador == 1) {
+                                    System.out.println("ingrese el nombre del heroe : ");
+                                    String nombre = str.nextLine();
+                                    String arma = "piocha";
+                                    int vida = 300;
+                                    int ataque = 350;
+                                    lista_heroes.add(new heroe(nombre, arma, vida, ataque));
+                                    heroe.explorar(lista_mapas, lista_heroes, lista_enemigos);
+                                    heroe.explorar(lista_mapas, lista_heroes, lista_enemigos, opc_jugador);
+
+                                } else if (opc_jugador == 0) {
+                                    System.out.println("se abandonara el menu de jugador");
+                                    salida_jugador = true;
+
+                                } else {
+                                    System.out.println("la opcion ingresada es invalida");
+                                }
+                            } while (salida_jugador != true);
 
                         }
 
@@ -189,22 +241,31 @@ public class Lab2p2_salvador_macias {
 
     public static void listar_mapas() {
         System.out.println("---lista de mapas---");
-        for (int i = 0; i < lista_mapas.size(); i++) {
-            System.out.println(
-                    lista_mapas.indexOf(lista_mapas.get(i)) + "-"
-                    + lista_mapas.get(i)
-            );
+        if (lista_mapas.isEmpty()) {
+            System.out.println("la lista de mapas esta vacia");
+        } else {
+            for (int i = 0; i < lista_mapas.size(); i++) {
+                System.out.println(
+                        lista_mapas.indexOf(lista_mapas.get(i)) + "-"
+                        + lista_mapas.get(i)
+                );
+            }
+
         }
 
     }
 
     public static void listar_enemigos() {
         System.out.println("---lista de enemigos---");
-        for (int i = 0; i < lista_enemigos.size(); i++) {
-            System.out.println(
-                    lista_enemigos.indexOf(lista_enemigos.get(i)) + "-"
-                    + lista_enemigos.get(i)
-            );
+        if (lista_enemigos.isEmpty()) {
+            System.out.println("la lista de enemigos esta vacia");
+        } else {
+            for (int i = 0; i < lista_enemigos.size(); i++) {
+                System.out.println(
+                        lista_enemigos.indexOf(lista_enemigos.get(i)) + "-"
+                        + lista_enemigos.get(i)
+                );
+            }
         }
 
     }
--- src/lab2p2_salvador_macias/heroe.java
+++ src/lab2p2_salvador_macias/heroe.java
@@ -4,6 +4,9 @@
  */
 package lab2p2_salvador_macias;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Apple
@@ -57,10 +60,64 @@ public class heroe {
         this.ataque = ataque;
     }
 
-    @Override
-    public String toString() {
-        return "nombre del heroe: "+nombre + "\narma: "+ arma + "\nvida: "+ vida + "\nataque: "+ataque;
+    public static Random random = new Random();
+    public static int aleatorio = random.nextInt(4);
+    public static int aleatorio_enemigos = random.nextInt(Lab2p2_salvador_macias.lista_enemigos.size());
+    public static int aleatorio_mapas = random.nextInt(Lab2p2_salvador_macias.lista_mapas.size());
+
+    public static void explorar(ArrayList<mapa> lista_mapas,ArrayList<heroe> lista_heroes,ArrayList<enemigo> lista_enemigos) {
+        if (Lab2p2_salvador_macias.lista_enemigos.isEmpty() && Lab2p2_salvador_macias.lista_mapas.isEmpty()) {
+            System.out.println("no se puede explorar debido a que la lista de mapas y enemigos esta vacia ");
+        } else {
+
+            if (aleatorio == 0) {
+                System.out.println("->" + Lab2p2_salvador_macias.lista_heroes.get(0).getNombre()
+                        + " se enfrento a un " + Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getNombre()
+                        + " en " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas).getZona() + " con ataque basico");
+
+                System.out.println("->" + Lab2p2_salvador_macias.lista_heroes.get(0).getNombre()
+                        + " le quito al enemigo " + Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getNombre()
+                        + Lab2p2_salvador_macias.lista_heroes.get(0).getAtaque() + " puntos de vida y recibio un dano de "
+                        + Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getAtaque()
+                );
+
+                System.out.println("->" + "por efectos del clima, el heroe perdio : " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas)
+                        .getEfecto_clima_heroe() + "puntos de vida y el enemigo " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas)
+                                .getEfecto_clima_enemigo());
+
+                //System.out.println("->" + "vida restante del heroe: "+Lab2p2_salvador_macias.lista_heroes.get(0).getVida()
+                //-Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getAtaque()+Lab2p2_salvador_macias.lista_mapas
+                //.get(aleatorio_mapas).getEfecto_clima_heroe());
+            } else if (aleatorio == 1 || aleatorio == 2 || aleatorio == 3) {
+                explorar(lista_mapas, lista_heroes, lista_enemigos, aleatorio);
+            }
+
+        }
+
     }
 
-    
+    public static void explorar(ArrayList<mapa> lista_mapas,ArrayList<heroe> lista_heroes,ArrayList<enemigo> lista_enemigos, int multiplicador) {
+        multiplicador = aleatorio;
+        multiplicador = Lab2p2_salvador_macias.lista_heroes.get(0).ataque * multiplicador;
+        System.out.println("->" + Lab2p2_salvador_macias.lista_heroes.get(0).getNombre()
+                + " se enfrento a un " + Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getNombre()
+                + " en " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas).getZona() + " con "
+        + Lab2p2_salvador_macias.lista_heroes.get(0).getArma());
+
+        System.out.println("->" + Lab2p2_salvador_macias.lista_heroes.get(0).getNombre()
+                + " le quito al enemigo " + multiplicador + " puntos de vida y recibio un dano de "
+                + Lab2p2_salvador_macias.lista_enemigos.get(aleatorio_enemigos).getAtaque()
+        );
+
+        System.out.println("->" + "por efectos del clima, el heroe perdio : " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas)
+                .getEfecto_clima_heroe() + "puntos de vida y el enemigo " + Lab2p2_salvador_macias.lista_mapas.get(aleatorio_mapas)
+                        .getEfecto_clima_enemigo());
+
+    }
+
+    @Override
+    public String toString() {
+        return "nombre del heroe: " + nombre + "\narma: " + arma + "\nvida: " + vida + "\nataque: " + ataque;
+    }
+
 }
