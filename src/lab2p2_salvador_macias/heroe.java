c23e00467f118fd836c7960e60e2db12d6367571
papilon10 <salvigna17@gmail.com>
2025-05-02 13:48:15 -0600

    clase heroe hecha

--- src/lab2p2_salvador_macias/heroe.java
+++ src/lab2p2_salvador_macias/heroe.java
@@ -15,4 +15,52 @@ public class heroe {
     private int vida;
     private int ataque;
 
+    public heroe() {
+    }
+
+    public heroe(String nombre, String arma, int vida, int ataque) {
+        this.nombre = nombre;
+        this.arma = arma;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre del heroe: "+nombre + "\narma: "+ arma + "\nvida: "+ vida + "\nataque: "+ataque;
+    }
+
+    
 }
