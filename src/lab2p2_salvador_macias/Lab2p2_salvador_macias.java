35a7bb7bc20c1528a37d7033b82efdec5c69c913
papilon10 <salvigna17@gmail.com>
2025-05-02 13:53:19 -0600

    menu vacio

--- src/lab2p2_salvador_macias/Lab2p2_salvador_macias.java
+++ src/lab2p2_salvador_macias/Lab2p2_salvador_macias.java
@@ -4,17 +4,43 @@
  */
 package lab2p2_salvador_macias;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Apple
  */
 public class Lab2p2_salvador_macias {
 
+    public static ArrayList lista = new ArrayList();
+    public static Scanner lea = new Scanner(System.in);
+    public static Scanner str = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean salida = false;
+        do {
+            System.out.println("---menu---");
+            System.out.println("1.zeldaRPG");
+            System.out.println("0.salir");
+            int opc = lea.nextInt();
+            if (opc == 1) {
+
+                System.out.println("a");
+            } else if (opc == 0) {
+                System.out.println("se abandonara el programa");
+                salida = true;
+
+            } else {
+                System.out.println("opcion ingresada es invalida");
+
+            }
+
+        } while (salida != true);
+
     }
-    
+
 }
