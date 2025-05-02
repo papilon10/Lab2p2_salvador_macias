3e3d3d62490aca024cd3d4f060583067a3063bfd
papilon10 <salvigna17@gmail.com>
2025-05-02 13:43:36 -0600

    clse enemigo hecha

--- src/lab2p2_salvador_macias/enemigo.java
+++ src/lab2p2_salvador_macias/enemigo.java
@@ -9,10 +9,47 @@ package lab2p2_salvador_macias;
  * @author Apple
  */
 public class enemigo {
+
     private String nombre;
     private int vida;
     private int ataque;
-    
-    
-    
+
+    public enemigo() {
+    }
+
+    public enemigo(String nombre, int vida, int ataque) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    @Override
+    public String toString() {
+        return "nombre del enemigo: " + nombre + "\n vida del enemigo:" + vida + "\nataque: " + ataque;
+    }
+
 }
